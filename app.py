515b21eeeacee34f0681cc7d6c73e7d262c66b48
Kostyan89 <kostasamin@gmail.com>
2021-11-01 17:34:38 +0200

    correct route movie/

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 import sqlite3
 
 
@@ -30,10 +30,9 @@ def movie_title():
     results = []
     name = request.args.get('s')
     if name:
-        sql = f"SELECT title, country, release_year, listed_in, description from netflix"\
-              f"WHERE lower(title) LIKE '%{name.lower()}%'ORDER BY release_year desc limit 1"
-
+        sql = f"SELECT title, country, release_year, listed_in, description from netflix WHERE lower(title) LIKE '%{name.lower()}%'ORDER BY release_year desc limit 1"
         results = run_sql(sql)
+    abort(404)
     return jsonify(make_result('title', 'country', 'genre', 'description', data=results)[0])
 
 
